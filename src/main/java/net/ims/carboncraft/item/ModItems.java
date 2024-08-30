4d802d32bd2535803fe86b7f3c25116b239f0931
KeinZweitAcc <m.greub.inf22@stud.bbbaden.ch>
2024-08-30 16:28:43 +0200

    added koks

--- src/main/java/net/ims/carboncraft/item/ModItems.java
+++ src/main/java/net/ims/carboncraft/item/ModItems.java
@@ -3,7 +3,6 @@ package net.ims.carboncraft.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.ims.carboncraft.CarbonCraft;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
