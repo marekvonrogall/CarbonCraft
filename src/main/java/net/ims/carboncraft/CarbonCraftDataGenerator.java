cef7d94aca11be92a604cb4a39d303ce5f6ec718
Tagesmeister <Pascal.Oestrich@outlook.de>
2024-08-30 15:10:06 +0200

    Set up Project

---
+++ src/main/java/net/ims/carboncraft/CarbonCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ims.carboncraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CarbonCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
