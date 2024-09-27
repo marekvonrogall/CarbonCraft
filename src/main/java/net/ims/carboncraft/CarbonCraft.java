9a60b56c1b7487469189f3aeed56b0da36c05986
m.greub.inf22 <M.Greub.inf22@stud.bbbaden.ch>
2024-09-27 15:49:50 +0200

    Added CrabonCraft Item Group in Creative Inventory

--- src/main/java/net/ims/carboncraft/CarbonCraft.java
+++ src/main/java/net/ims/carboncraft/CarbonCraft.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.ims.carboncraft.block.ModBlocks;
 import net.ims.carboncraft.item.ModArmorMaterials;
+import net.ims.carboncraft.item.ModItemGroups;
 import net.ims.carboncraft.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,7 @@ public class CarbonCraft implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 
